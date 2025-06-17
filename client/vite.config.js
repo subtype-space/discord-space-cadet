6af958e8c4c8d520177b25d835b0adf547390589
subtype <andrew.subowo@gmail.com>
2025-06-16 21:44:43 -0400

    test push

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,25 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../',
+  envDir: "../",
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
         ws: true,
+        rewrite: (path) => path.replace(/^\/\.proxy/, ""),
       },
     },
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+    allowedHosts: true,
     hmr: {
       clientPort: 443,
     },
   },
-});
+})
